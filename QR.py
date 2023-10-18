d42bccb6f979264e2f5081d250349dc7653ee4e3
mynameiscr7 <146122567+mynameiscr7@users.noreply.github.com>
2023-10-18 14:03:51 +0800

    Create QR.py

---
+++ QR.py
@@ -0,0 +1,21 @@
+from MyQR import myqr
+import os
+import datetime
+
+url = "https://www.aliwork.com/s/desktopQR?seat="
+path = f"{os.environ['USERPROFILE']}\\Desktop\\qr\\"
+if not os.path.exists(path):
+    os.makedirs(path)
+print(datetime.datetime.now())
+with open('seat.txt', 'r', encoding='utf-8') as fr:
+    for seat in fr.readlines():
+        seat = seat.strip()
+        #print(seat)
+        myqr.run(
+            words=f'{url}{seat}',
+            save_name=f"{seat}.png",
+            save_dir=path,
+            # picture="工位标识.png",
+            colorized=True,
+        )
+print(datetime.datetime.now())
